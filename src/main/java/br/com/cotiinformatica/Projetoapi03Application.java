1140c4e7ae53e11cd155e375dcc702951f2c2388
romildolopes174@gmail.com <romildolopes174@gmail.com>
2023-06-17 21:40:09 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/Projetoapi03Application.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetoapi03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetoapi03Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/cotiinformatica/controllers/AcessarContaController.java
@@ -0,0 +1,22 @@
+package br.com.cotiinformatica.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cotiinformatica.dtos.AcessarContaPostDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "Acessar Conta de usuário")
+@RestController
+public class AcessarContaController {
+	
+	@ApiOperation("Serviço para autenticação de usuáario.")
+	@PostMapping("/api/acessar-conta")
+	public void post(@RequestBody AcessarContaPostDto dto) {
+		// TODO
+		
+	}
+
+}
